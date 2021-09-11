02dd81003e0a2d7a94fc729580f9f1f6e1195732
Angel Gomez <agomez@gteam.es>
2021-09-11 22:27:35 +0200

    Organización de las carpetas del proyecto y ejemplo de StreamWritter.

---
+++ Writter-Kata.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using System;
+using Xunit;
+
+namespace Writter_Kata.Tests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
--- Writter-Kata/Program.cs
+++ Writter-Kata/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.IO;
 
 namespace Writter_Kata {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            DirectoryInfo[] cDirs = new DirectoryInfo(@"c:\").GetDirectories();
+            // Write each directory name to a file.
+            using(StreamWriter sw = new StreamWriter("CDriveDirs.txt")) {
+                foreach(DirectoryInfo dir in cDirs) {
+                    sw.WriteLine(dir.Name);
+                }
+            }
+            // Read and show each line from the file.
+            string line = "";
+            using(StreamReader sr = new StreamReader("CDriveDirs.txt")) {
+                while((line = sr.ReadLine()) != null) {
+                    Console.WriteLine(line);
+                }
+            }
         }
     }
 }
